6a21f5c9eba0c8e8211a5848bab109b2980b564f
Yanisdje <64096824+Yanisdje@users.noreply.github.com>
2024-08-23 15:45:27 -0400

    Added backend code

---
+++ backend/api/prayers.api.js
@@ -0,0 +1,16 @@
+const getPrayers = async (req, res) => {
+    try{
+        const url = "https://api.aladhan.com/v1/timingsByCity/23-08-2024?city=Montreal&country=Canada&method=4&adjustment=1";
+        const response = await fetch(url);
+        if(response.ok){
+            const data =await response.json();
+            res.status(200).json(data);
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({message: err.message});
+    }
+};
+
+module.exports = {getPrayers};
