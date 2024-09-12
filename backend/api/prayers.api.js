9c41a2762be882fd40566398e498fc6e7861b508
Yassine_Ibhir <yibhir101@gmail.com>
2024-09-11 21:02:50 -0400

    added endpoint for prayers/jumaa/imam

--- backend/api/prayers.api.js
+++ backend/api/prayers.api.js
@@ -1,4 +1,6 @@
 const firebase = require("../firebase.js");
+
+
 const {
   getFirestore,
   collection,
@@ -9,13 +11,19 @@ const {
   updateDoc,
   deleteDoc,
   setDoc,
-} = require('firebase/firestore'); ;
+} = require('firebase/firestore');;
 
 const data = require("../data/prayers.js");
+const imams = require("../data/imams.js");
+const daily = require("../data/daily.js");
 const db = getFirestore(firebase);
 
-
-const storePrayers = async (req, res) => {
+/**
+ * Store weekly prayers in the database
+ * @param {*} req 
+ * @param {*} res 
+ */
+const storeWeeklyPrayers = async (req, res) => {
   try {
     const docRef = doc(db, "prayers", "weekly");
     await setDoc(docRef, data, { merge: true });
@@ -26,7 +34,136 @@ const storePrayers = async (req, res) => {
   }
 };
 
+/**
+ * store daily prayers in the database
+ * @param {*} req 
+ * @param {*} res 
+ */
+const storeDailyPrayers = async (req, res) => {
+  try {
 
+    const prayers = req.body;
+    const docRef = doc(db, "prayers", "daily");
+    // await setDoc(docRef, daily, { merge: true });
+    await setDoc(docRef, prayers, { merge: true });
+
+    res.status(200).send("Prayers added to database");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+/**
+ * Store imams in the database.
+ * Every imams will be updated in the database.
+ * @param {*} req 
+ * @param {*} res 
+ */
+const storeImams = async (req, res) => {
+
+  try {
+    const docRef = doc(db, "prayers", "imams");
+    await setDoc(docRef, imams, { merge: true });
+    res.status(200).send("Imams are added to database");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+
+};
+
+/**
+ * Get daily prayers from the database
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getDailyPrayers = async (req, res) => {
+
+  try {
+    const docRef = doc(db, "prayers", "daily");
+    const response = await getDoc(docRef, daily);
+    const prayerArray = convertObjectToArray(response.data());
+    res.status(200).send(prayerArray);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+
+};
+
+
+/**
+ * Get jumaa prayer from the database
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getJumaaPrayer = async (req, res) => {
+  try {
+    const docRef = doc(db, "prayers", "daily");
+    const response = await getDoc(docRef);
+    const jumaa = response.data().Jumaa;
+    res.status(200).send(jumaa);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+
+};
+
+/**
+ * Get upcoming prayer based on data in db
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getNextPrayer = async (req, res) => {
+
+  try {
+    const docRef = doc(db, "prayers", "daily");
+
+    const response = await getDoc(docRef);
+
+    const prayerArray = convertObjectToArray(response.data());
+
+    //remove jumaa from the array
+    prayerArray.pop();
+
+    const timeStampsArr = getArrayOfTimeStamps(prayerArray);
+
+    const timeStamp = getCurrentTimestamp();
+
+    const upcomingIndex = upcomingPrayerIndex(timeStampsArr, timeStamp);
+
+    res.status(200).json(prayerArray[upcomingIndex]);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+/**
+ * get imam from db
+ * @param {day,prayer} req 
+ * @param {*} res 
+ */
+
+const getImam = async (req, res) => {
+
+  try {
+    const { day, prayer } = req.query;
+    const docRef = doc(db, "prayers", "imams");
+    const response = await getDoc(docRef);
+    const imamsData = response.data();
+    const imam = imamsData[day][prayer];
+    res.status(200).json(imam);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+};
 
 const getPrayers = async (req, res) => {
   try {
@@ -46,7 +183,7 @@ const getPrayers = async (req, res) => {
   }
 };
 
- const getJumaah = async (req, res) => {
+const getJumaah = async (req, res) => {
   try {
     const { date, city, country, method, adjustment } = req.query;
     const apiUrl = `${process.env.PRAYER_URL_API}/${date}?city=${city}&country=${country}&method=${method}&adjustment=${adjustment}`;
@@ -61,7 +198,7 @@ const getPrayers = async (req, res) => {
   }
 };
 
- const getUpcomingPrayer = async (req, res) => {
+const getUpcomingPrayer = async (req, res) => {
   try {
     const { date, city, country, method, adjustment } = req.query;
     const apiUrl = `${process.env.PRAYER_URL_API}/${date}?city=${city}&country=${country}&method=${method}&adjustment=${adjustment}`;
@@ -84,10 +221,10 @@ const getPrayers = async (req, res) => {
 
 const upcomingPrayerIndex = (prayers, currentTime) => {
   // Sort the array in ascending order
-  prayers.sort((a, b) => a - b);
+  const sortedArray = prayers.sort((a, b) => a - b);
 
-  for (let i = 0; i < prayers.length - 1; i++) {
-    if (currentTime >= prayers[i] && currentTime <= prayers[i + 1]) {
+  for (let i = 0; i < sortedArray.length - 1; i++) {
+    if (currentTime >= sortedArray[i] && currentTime <= sortedArray[i + 1]) {
       return i + 1;
     }
   }
@@ -101,18 +238,18 @@ const getCurrentTimestamp = () => {
 const timeStamps = (timeStrings) => {
   return timeStrings.map((timeString) => {
     const [hours, minutes] = timeString[1].split(":").map(Number);
-
     const now = new Date();
-
     now.setHours(hours, minutes, 0, 0);
-
     return now.getTime();
   });
 };
 
+
+
 const filterPrayerTimes = (prayerTimes) => {
   // Convert the object into an array of key-value pairs
   const entries = Object.entries(prayerTimes);
+
 
   // Exclude the last 4 elements and filter out the second (index 1) and fifth (index 4) elements
   const filteredEntries = entries
@@ -124,5 +261,33 @@ const filterPrayerTimes = (prayerTimes) => {
 };
 
 
+const convertObjectToArray = (obj) => {
 
-module.exports = { getPrayers, getUpcomingPrayer, getJumaah, storePrayers };
+  const arr = [
+    ["Fajr", obj["Fajr"]],
+    ["Dhuhr", obj["Dhuhr"]],
+    ["Asr", obj["Asr"]],
+    ["Maghrib", obj["Maghrib"]],
+    ["Isha", obj["Isha"]],
+    ["Jumaa", obj["Jumaa"]],
+  ];
+
+  return arr;
+}
+
+const getArrayOfTimeStamps = (timeStrings) => {
+
+  console.log(timeStrings);
+  return timeStrings.map((timeString) => {
+    const [hours, minutes] = timeString[1].Iqama.split(":").map(Number);
+
+    const now = new Date();
+
+    now.setHours(hours, minutes, 0, 0);
+
+    return now.getTime();
+  });
+};
+
+
+module.exports = { getPrayers, getUpcomingPrayer, getJumaah, storeWeeklyPrayers, storeImams, storeDailyPrayers, getDailyPrayers, getJumaaPrayer, getNextPrayer, getImam };
--- backend/routes/prayers.route.js
+++ backend/routes/prayers.route.js
@@ -1,17 +1,42 @@
 const express = require("express");
 
-const { getPrayers, getJumaah, getUpcomingPrayer, storePrayers } = require("../api/prayers.api");
+const {
+    getPrayers,
+    getJumaah,
+    getUpcomingPrayer,
+    storeWeeklyPrayers,
+    storeImams,
+    storeDailyPrayers,
+    getDailyPrayers,
+    getJumaaPrayer,
+    getNextPrayer,
+    getImam,
+} = require("../api/prayers.api");
 
 
 const PrayerRouter = express.Router();
 
 PrayerRouter.get("/dailyprayers", getPrayers);
 
-PrayerRouter.get("/jumaaprayer", getJumaah);
+PrayerRouter.get("/jumaahprayer", getJumaah);
 
 PrayerRouter.get("/upcomingprayer", getUpcomingPrayer);
 
-PrayerRouter.post("/storeprayers", storePrayers);
+PrayerRouter.get("/nextprayer", getNextPrayer);
+
+PrayerRouter.get("/jumaaprayer", getJumaaPrayer);
+
+PrayerRouter.get("/daily", getDailyPrayers);
+
+PrayerRouter.get("/imam", getImam);
+
+PrayerRouter.post("/storeweeklyprayers", storeWeeklyPrayers);
+
+PrayerRouter.post("/storeimams", storeImams);
+
+PrayerRouter.post("/storedailyprayers", storeDailyPrayers);
+
+
 
 
 module.exports = PrayerRouter;
