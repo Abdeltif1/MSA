5eca6931946a3abbc955612c989e42da8fa9b3dc
Yanisdje <64096824+Yanisdje@users.noreply.github.com>
2024-09-16 16:24:13 -0400

    Formatted the data

--- web-frontend/src/components/Menu/IqamaTable.jsx
+++ web-frontend/src/components/Menu/IqamaTable.jsx
@@ -2,32 +2,26 @@ import React, { useState } from "react";
 import { CDBTable, CDBTableHeader, CDBTableBody, CDBContainer, CDBBtn } from "cdbreact";
 
 export default function IqamaImamTable() {
-  const [iqamaTimes, setIqamaTime] = useState({
-    Fajr: "5", // Default time
-    Dhuhr: "5",
-    Asr: "5",
-    Maghrib: "5",
-    Isha: "5",
-  });
-  //Make a function for the iqama time + prayer.
+  const [iqamaTimes, setIqamaTimes] = useState([
+    { fajr: 5 },
+    { dhuhr: 5 },
+    { asr: 5 },
+    { maghrib: 5 },
+    { isha: 5 },
+  ]);
 
-  const handleIqamaTime = (choice, prayer) => {
-    const selectedTime = choice.target.value;
-    setIqamaTime(selectedTime);
-    console.log(selectedTime, prayer);
+  const handleIqamaTime = (choice, index) => {
+    const selectedTime = parseInt(choice.target.value);
+    setIqamaTimes((prevTimes) => {
+      const updatedTimes = [...prevTimes];
+      const key = Object.keys(updatedTimes[index])[0];  
+      updatedTimes[index] = { [key]: selectedTime };  
+      return updatedTimes; 
+    });
   };
-
-  const prayers = ["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"];
 
   return (
     <CDBContainer>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "150px",
-        }}
-      ></div>
       <CDBTable bordered>
         <CDBTableHeader>
           <tr>
@@ -36,26 +30,34 @@ export default function IqamaImamTable() {
           </tr>
         </CDBTableHeader>
         <CDBTableBody>
-          {prayers.map((prayer, index) => (
-            <tr key={index}>
-              <td>
-                <strong>{prayer}</strong>
-              </td>
-              <td>
-                <select name="times" id="times" onChange={(choice) => handleIqamaTime(choice, prayer)}>
-                  <option value="5">5 mins</option>
-                  <option value="10">10 mins</option>
-                  <option value="15">15 mins</option>
-                  <option value="20">20 mins</option>
-                </select>
-              </td>
-            </tr>
-          ))}
+          {iqamaTimes.map((item, index) => {
+            const prayer = Object.keys(item)[0]; 
+            return (
+              <tr key={index}>
+                <td>
+                  <strong>{prayer.charAt(0).toUpperCase() + prayer.slice(1)}</strong>
+                </td>
+                <td>
+                  <select
+                    name={`times-${prayer}`}
+                    id={`times-${prayer}`}
+                    value={item[prayer]}
+                    onChange={(choice) => handleIqamaTime(choice, index)}
+                  >
+                    <option value={5}>5 mins</option>
+                    <option value={10}>10 mins</option>
+                    <option value={15}>15 mins</option>
+                    <option value={20}>20 mins</option>
+                  </select>
+                </td>
+              </tr>
+            );
+          })}
         </CDBTableBody>
       </CDBTable>
 
-      <div style={{ marginTop: '20px', textAlign: 'center' }}>
-        <CDBBtn color="primary" >
+      <div style={{ marginTop: "20px", textAlign: "center" }}>
+        <CDBBtn color="primary" onClick={() => console.log(iqamaTimes)}>
           Submit Iqama Times
         </CDBBtn>
       </div>
